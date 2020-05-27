695582d17c239bb3ac053c8d47072c2f02600e4f
zweistein1326 <40302363+zweistein1326@users.noreply.github.com>
2020-05-27 17:12:47 +0530

    update products_overview_screen.dart

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -6,30 +6,15 @@ import '../providers/products.dart';
 import '../widgets/product_tile.dart';
 import '../widgets/app_drawer.dart';
 
-class ProductsOverviewScreen extends StatefulWidget {
+class ProductsOverviewScreen extends StatelessWidget {
   static const routeName = '/products-screen';
 
-  @override
-  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
-}
-
-class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
-  int index = 0;
   final searchState = false;
-  void _selectPage(i) {
-    setState(() {
-      index = i;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     var products = Provider.of<Products>(context).items('');
-    void searchByItem(searchString) {
-      setState(() {
-        products = Provider.of<Products>(context).items(searchString);
-      });
-    }
+    void searchByItem(searchString) {}
 
     return Scaffold(
       drawer: AppDrawer(),
@@ -55,58 +40,34 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
                   ),
                 ],
               ))),
-      body: index == 0
-          ? searchState
-              ? null
-              : Column(
-                  children: <Widget>[
-                    SearchBar(searchByItem),
-                    Container(
-                      height: MediaQuery.of(context).size.height * 0.3,
-                      alignment: Alignment.center,
-                      child: Image.network(
-                        'https://www.telegraph.co.uk/content/dam/food-and-drink/2018/06/20/Veg_trans_NvBQzQNjv4Bqul3YgLXf2lEf3afmzmy4CHMT9HEIQzXrohIUnOkGrQA.jpg',
-                      ),
-                    ),
-                    Container(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 0, vertical: 10),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black12),
-                      ),
-                      height: MediaQuery.of(context).size.height * 0.23,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemBuilder: (ctx, i) => ProductTile(products[i]),
-                        itemCount: products.length,
-                      ),
-                    ),
-                    Expanded(
-                        child: Column(
-                      children: <Widget>[
-                        Text(
-                            'This is the unknown section?? What should I add here')
-                      ],
-                    ))
-                  ],
-                )
-          : Center(
-              child: Text("Favorites"),
+      body: Column(
+        children: <Widget>[
+          SearchBar(searchByItem),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.3,
+            alignment: Alignment.center,
+            child: Image.network(
+              'https://www.telegraph.co.uk/content/dam/food-and-drink/2018/06/20/Veg_trans_NvBQzQNjv4Bqul3YgLXf2lEf3afmzmy4CHMT9HEIQzXrohIUnOkGrQA.jpg',
             ),
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: _selectPage,
-        currentIndex: index,
-        elevation: 10,
-        backgroundColor: Colors.white,
-        items: [
-          BottomNavigationBarItem(
-            title: Text('Home'),
-            icon: Icon(Icons.home),
           ),
-          BottomNavigationBarItem(
-              title: Text('Favorites'),
-              icon: Icon(Icons.favorite_border),
-              activeIcon: Icon(Icons.favorite)),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 0, vertical: 10),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black12),
+            ),
+            height: MediaQuery.of(context).size.height * 0.23,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (ctx, i) => ProductTile(products[i]),
+              itemCount: products.length,
+            ),
+          ),
+          Expanded(
+              child: Column(
+            children: <Widget>[
+              Text('This is the unknown section?? What should I add here')
+            ],
+          ))
         ],
       ),
     );
